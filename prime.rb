fe06ed2f695d18691e50acbf413e8e4d7c4ae33f
Haim D <haim11293@aol.com>
2019-06-26 20:33:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(integer)
-  starting_integer = 4
+  starting_integer = 2
   if integer > 1
     range = (starting_integer..integer-1).to_a
     range.none? do |testing_integer|
